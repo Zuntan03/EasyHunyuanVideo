392eb27ef5fcb36a251e6ef077fe139a75fe80a5
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2025-01-11 20:48:45 +0900

    TIPOのモデルバージョン更新

--- Download/kgen/TIPO-200M-ft2_TIPO-200M-ft2-F16.bat
+++ Download/kgen/TIPO-200M-ft2_TIPO-200M-ft2-F16.bat
@@ -4,8 +4,8 @@ set EASY_TOOLS=%~dp0..\..\EasyTools
 set HUGGING_FACE=%EASY_TOOLS%\Download\HuggingFace.bat
 pushd %~dp0..\..\ComfyUI\models\kgen
 
-@REM https://huggingface.co/KBlueLeaf/TIPO-200M-ft
-call %HUGGING_FACE% .\ TIPO-200M-ft_TIPO-200M-ft-F16.gguf KBlueLeaf/TIPO-200M-ft TIPO-200M-ft-F16.gguf?
+@REM https://huggingface.co/KBlueLeaf/TIPO-200M-ft2
+call %HUGGING_FACE% .\ TIPO-200M-ft2_TIPO-200M-ft2-F16.gguf KBlueLeaf/TIPO-200M-ft2 TIPO-200M-ft2-F16.gguf?
 if %ERRORLEVEL% neq 0 ( popd & exit /b 1 )
 
 popd rem %~dp0..\..\ComfyUI\models\kgen
